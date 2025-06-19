479a2a781c26c59e474a6b3a518277630fdea310
Vadym Kobylynskyi <vadimmm972@gmail.com>
2025-06-19 09:01:43 +0300

    Update VoiceRecorder.vue

--- src/views/VoiceRecorder.vue
+++ src/views/VoiceRecorder.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="voice-recorder">
-    <button @click="startRecording" :disabled="recording">🎤 Start recording</button>
-    <button @click="stopRecording" :disabled="!recording">■ Stop</button>
-    <p v-if="audioUrl">
-      <audio :src="audioUrl" controls></audio>
-    </p>
-    <p v-if="error" style="color: red">{{ error }}</p>
+  <div>
+    <button @click="startRecording" :disabled="recording">Start recording</button>
+    <button @click="stopRecording" :disabled="!recording">Stop</button>
+    <audio v-if="audioUrl" :src="audioUrl" controls></audio>
+    <div v-if="error" style="color: red">{{ error }}</div>
   </div>
 </template>
 
@@ -21,55 +19,48 @@ export default {
     }
   },
   methods: {
-startRecording() {
-  this.error = '';
-  this.audioUrl = '';
-  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    this.error = 'getUserMedia is not supported in this browser';
-    return;
-  }
-  navigator.mediaDevices.getUserMedia({ audio: true })
-    .then(stream => {
-      let mimeType = '';
-      if (MediaRecorder.isTypeSupported('audio/mp4')) {
-        mimeType = 'audio/mp4';
-      } else if (MediaRecorder.isTypeSupported('audio/aac')) {
-        mimeType = 'audio/aac';
-      } else if (MediaRecorder.isTypeSupported('audio/webm')) {
-        mimeType = 'audio/webm';
-      } else {
-        mimeType = '';
+    startRecording() {
+      this.error = '';
+      this.audioUrl = '';
+      if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        this.error = 'getUserMedia is not supported in this browser';
+        return;
       }
-      this.mediaRecorder = new window.MediaRecorder(stream, mimeType ? { mimeType } : undefined);
-      this.audioChunks = [];
-      this.mediaRecorder.ondataavailable = e => this.audioChunks.push(e.data);
-      this.mediaRecorder.onstop = this.onStop;
-      this.mediaRecorder.start();
-      this.recording = true;
-    })
-    .catch(err => {
-      this.error = 'Microphone access denied or unavailable: ' + err;
-    });
-},
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+          let mimeType = '';
+          if (window.MediaRecorder.isTypeSupported('audio/mp4')) {
+            mimeType = 'audio/mp4';
+          } else if (window.MediaRecorder.isTypeSupported('audio/aac')) {
+            mimeType = 'audio/aac';
+          } else {
+            mimeType = '';
+          }
+          this.mediaRecorder = new window.MediaRecorder(stream, mimeType ? { mimeType } : undefined);
+          this.audioChunks = [];
+          this.mediaRecorder.ondataavailable = e => this.audioChunks.push(e.data);
+          this.mediaRecorder.onstop = this.handleStop;
+          this.mediaRecorder.start();
+          this.recording = true;
+        })
+        .catch(err => {
+          this.error = 'Microphone access denied or unavailable: ' + err;
+        });
+    },
     stopRecording() {
       if (this.mediaRecorder && this.recording) {
         this.mediaRecorder.stop();
         this.recording = false;
       }
     },
-    onStop() {
-      const audioBlob = new Blob(this.audioChunks, { type: 'audio/webm' });
-      this.audioUrl = URL.createObjectURL(audioBlob);
-
-      // this.sendToServer(audioBlob);
-    },
-    // sendToServer(audioBlob) { ... }
+    handleStop() {
+      try {
+        const audioBlob = new Blob(this.audioChunks, { type: this.mediaRecorder.mimeType || 'audio/mp4' });
+        this.audioUrl = URL.createObjectURL(audioBlob);
+      } catch (e) {
+        this.error = 'Error while processing audio: ' + e.message;
+      }
+    }
   }
 }
 </script>
-
-<style scoped>
-.voice-recorder {
-  margin: 20px 0;
-}
-</style>
