0add62ad172b08ac0ee31043bdcd489213c5905e
Vadym Kobylynskyi <vadimmm972@gmail.com>
2025-09-10 19:54:18 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,55 @@
-<template>
+<!---<template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-     <!-- <Version2 /> -->
      <VoiceRecorderWithAssembly />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-//import Version2 from './components/Version2.vue'
-//import VoiceRecorder from './views/VoiceRecorder.vue';
 import VoiceRecorderWithAssembly from './views/VoiceRecorderWithAssembly.vue';
 export default {
   name: 'App',
   components: {
     VoiceRecorderWithAssembly
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+
+
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+<TalkingReply
+  :videoSrc="require('./assets/avatar_2.mp4')"
+  :posterSrc="require('./assets/avatar.png')"
+  lang="en-US"
+/>
+
+
+
+  </div>
+</template>
+
+<script>
+import TalkingReply from './views/TalkingReply.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TalkingReply
   }
 }
 </script>
